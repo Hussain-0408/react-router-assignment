9e825fdb5f67ef2b6649a222b05c60270af9b5b1
Hussain <althafhusssaian@gmail.com>
2025-09-23 19:58:39 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
-import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import About from './components/About';
 import PageNotfound from './components/PageNotfound';
+import Users from './components/Users';
+
 
 function App() {
- const navigate = useNavigate();
   return (
-    <div className="App">
+    <div className="App-container ">
       <Navbar />
       <Routes>
-        
-        <Route path='/' element={<Home/>} />
-        <Route path='/About' element={<About/>} />
-        <Route path='/Contact' element={<Contact/>} />
-        <Route path ='*' element ={<PageNotfound /> } />
+
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+        <Route path='*' element={<PageNotfound />} />
+        <Route path='/Users' element={<Users />} />
+
       </Routes>
-      {/* <button onClick={()=> navigateTowhere()}>About</button> */}
-       {/* <button onClick={()=> navigateTo('/Contact')}>About</button> */}
-        <button onClick={()=>navigate(-1)}> back</button>
-      
+
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,53 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import '../styles/Navbar.css'
+import logo from './image/logo.jpg'
+
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-    const NavLinkcss = ({isActive})=>{
-        return{
-            color: isActive ? "green" : "red",
-            fontsize: isActive ? "10px" : "20px",
-
-        }
-    }
-
   return (
     <div>
-        <h1>
-            <NavLink style = {NavLinkcss} to='/'>Home</NavLink>
-            
-            <NavLink style = {NavLinkcss} to ='/About'>About</NavLink>
-            <NavLink style = {NavLinkcss} to='/Contact'>Contact</NavLink>
-        </h1>
+      <nav className="navbar shadow-lg navbar-expand-lg fixed-top navbar-dark custom-navbar">
+        <div className="container-fluid">
+        
+          <Link className="navbar-brand d-flex  w-25  align-items-center" to="/">
+            <img 
+              src= {logo}
+              alt="Logo"
+              width="50"
+              height="50"
+              className="d-inline-block align-text-top me-3 rounded-3"
+            />
+            <span className="school-name">My App</span>
+          </Link>
+
+          {/* Toggler for mobile */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Navbar Links */}
+          <div className="collapse navbar-collapse  w-50 justify-content-center" id="navbarNav">
+            <ul className="navbar-nav  justify-content-around align-item-center w-50 gap-5">
+              <li className="nav-item">
+                <Link className="nav-link " to="/">Home</Link>
+              </li>
+              <li className="nav-item"><Link className="nav-link" to="/About">About</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/user">User Info</Link></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
 export default Navbar
