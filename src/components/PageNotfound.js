eacb7f571cff7012c896cd953ac5ce5e475c38d7
Hussain <althafhusssaian@gmail.com>
2025-09-24 12:21:04 +0530

    update pagefound page

--- src/components/PageNotfound.js
+++ src/components/PageNotfound.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import '../styles/Page.css'
 import error from './image/error.jpg';
+import {useNavigate} from 'react-router-dom';
 
 function PageNotfound() {
+
+  const goback = useNavigate();
   return (
     <div className='error-container container w-100 h-100'>
       <div className='error-childcontainer d-flex align-items-center justify-content-center  w-100 h-100'>
@@ -10,17 +13,17 @@ function PageNotfound() {
           <div className='error-heading w-100 text-center  p-3 '>
             <h1 >404 - Page Not Found</h1>
           </div>
-          <div className='error-information container  w-100 p-2 text-center justify-content-center  d-flex flex-row'>
-            <div className='error-img   '>
+          <div className='error-information   w-75 p-2 text-center  justify-content-arround align-items-start   d-flex flex-row'>
+            <div className='error-img w-50   '>
               <img src={error} alt=''   />
             </div>
-            <div className='error-info d-flex w-50  align-items-center text-center'>
-              <div className=' w-100 d-flex flex-column align-item-start'>
-                <div className='w-100'>
-                  <h5 className='p-3'> Oops! The page You're looking for doesn't exist.</h5> 
+            <div className='error-info w-100 d-flex  justify-content-center  align-items-center  text-center'>
+              <div className=' w-100 d-flex flex-column '>
+                <div className='100'>
+                  <h5 className='p-3 '> Oops! The page You're looking for doesn't exist.</h5> 
                 </div>
                 <div className='w-100'>
-                  <button className='p-1 rounded-3 bg-primary text-white fs-5'>Go to Home Page</button>
+                  <button className='p-2 rounded-3  text-white fs-5' onClick={()=>goback('/')}>Go to Home Page</button>
                   </div>
               </div>
             </div>
